53849be5759feadc653fef4983a030233765c8f5
David Basantes <sebastian2405lucero@hotmail.com>
2024-10-10 21:56:57 -0500

    Product Microservices NAST

---
+++ src/config/envs.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+  DATABASE_URL: string;
+  NATS_SERVERS: string[];
+}
+
+const envSchemas = joi
+  .object({
+    PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
+  })
+  .unknown(true);
+
+const { error, value } = envSchemas.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+  databaseUrl: envVars.DATABASE_URL,
+  natsServer: envVars.NATS_SERVERS,
+};
